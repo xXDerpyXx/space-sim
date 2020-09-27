9f09ce7a1521881a22e613ce25bc1086d412da5e
flynnA <flynnamos@hotmail.com>
2020-09-27 16:51:30 +0100

    botched direct connect feature

--- src/serverbrowser/index.js
+++ src/serverbrowser/index.js
@@ -5,6 +5,21 @@ import connect from './connect';
 import d from '../d';
 import serverList from './serverlist';
 
+function directConnect() {
+    let address = document.getElementById('toConnect').value;
+    let server = {
+        url: address,
+        secure: false,
+    };
+    if (address.startsWith('http://')) {
+        server.url = address.slice('http://'.length);
+    } else if (address.startsWith('https://')) {
+        server.url = address.slice('https://'.length);
+        server.secure = true;
+    }
+    connect(server);
+}
+
 function ServerBrowser() {
     if (d.socket != null) {
         d.socket.close();
@@ -26,6 +41,14 @@ function ServerBrowser() {
     return (
         <div>
             {servers}
+            <br />
+            <label>Direct connect:</label>
+            <br />
+            <input id="toConnect" placeholder="IP/Address" onKeyUp={e => {
+                if (e.key === 'Enter')
+                    directConnect();
+            }} />
+            <button onClick={directConnect}>Connect</button>
         </div>
     );
 }
