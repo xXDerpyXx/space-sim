fab1f7a560a6b8f998b96fa6486a2f2fed8d102b
flynnA <flynnamos@hotmail.com>
2020-09-28 13:54:17 +0100

    clear lol

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import ServerBrowser from './serverbrowser';
-import Game from './game';
-import d from './d';
-
-ReactDOM.render(ServerBrowser(), d.root);
