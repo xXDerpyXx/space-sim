3251eff1fee9dec9c723f3b688318954773ccd91
flynnA <flynnamos@hotmail.com>
2020-12-04 21:45:40 +0000

    block first chat notification

--- src/game/chat/receive.js
+++ src/game/chat/receive.js
@@ -16,7 +16,7 @@ function receiveMessages() {
         }
         document.getElementById('messagesSentNum').innerHTML = children.length; //display the amount of messages sent on the open chat button
 
-        if (!chatIsShown()) //if the chat box is not open (and therefore the user is not aware that a new message has been posted)
+        if (!chatIsShown() && children.length > 1) //if the chat box is not open (and therefore the user is not aware that a new message has been posted)
             document.getElementById('openChatButton').style.background = '#f56262'; //change the colour of the open chat button, to make it more obvious to the user that a message has been sent.
     });
 }
